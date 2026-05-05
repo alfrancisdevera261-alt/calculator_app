b1db95f9b0bec39af0007a7f43b85876f5b27763
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-05-05 23:14:40 +0800

    created a error when the user input is not a number

--- operations/exceptions.py
+++ operations/exceptions.py
@@ -7,8 +7,12 @@ class InvalidOperationError(CalculatorError):
     def __init__(self, choice):
         super().__init__(f"'{choice}' is not a valid operation.")
         
-
 class DivisionByZeroError(CalculatorError):
     """Raised when dividing by zero"""
     def __init__(self):
         super().__init__("Cannot divide by zero.")
+        
+class InvalidNumberError(CalculatorError):
+    """Raised when input is not a number"""
+    def __init__(self, value):
+        super().__init__(f"'{value}' is not a valid number.")
