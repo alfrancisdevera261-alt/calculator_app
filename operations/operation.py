0004b087d9922bd7fb85ffe9338dec56aef1e917
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-05-05 23:47:31 +0800

    created a abstract class that is a template that can't be used directly, but forces all child classes to implement the same required methods to ensure consistency.

--- operations/operation.py
+++ operations/operation.py
@@ -1,3 +1,15 @@
 from abc import ABC, abstractmethod
 
 from operations.exceptions import DivisionByZeroError
+
+class Operation(ABC):
+    """
+    Abstract class: cannot be instantiated directly
+    All child classes must implement execute()
+    """
+    symbol = ""
+    name = ""
+
+    @abstractmethod
+    def execute(self, a, b):
+        pass  # forces child classes to implement this
