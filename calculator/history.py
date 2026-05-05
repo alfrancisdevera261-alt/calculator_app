8f82262dd8453c553b4bf38aa0a151a33e08eb83
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-05-05 23:36:23 +0800

    make a format in the ouput for the history

--- calculator/history.py
+++ calculator/history.py
@@ -5,3 +5,6 @@ class HistoryEntry:
         self.expression = expression
         self.result = result
         self.timestamp = datetime.now().strftime("%H:%M:%S")
+
+    def __str__(self):
+        return f"[{self.timestamp}] {self.expression} = {self.result}"
