d8dba2f080bd0ddd520a6c7bc9f06445d71b00be
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-05-05 23:35:28 +0800

    created a class to store each calculation

--- calculator/history.py
+++ calculator/history.py
@@ -1,1 +1,7 @@
 from datetime import datetime
+
+class HistoryEntry:
+    def __init__(self, expression, result):
+        self.expression = expression
+        self.result = result
+        self.timestamp = datetime.now().strftime("%H:%M:%S")
