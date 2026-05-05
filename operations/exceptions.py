383ea8b82e57ddd729d8138b38a2ed5c95679c13
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-05-05 23:10:05 +0800

    created a error for when user selects invalid menu option

--- operations/exceptions.py
+++ operations/exceptions.py
@@ -1,3 +1,8 @@
 class CalculatorError(Exception):
     """Base class for all calculator-related errors"""
     pass
+
+class InvalidOperationError(CalculatorError):
+    """Raised when user selects invalid menu option"""
+    def __init__(self, choice):
+        super().__init__(f"'{choice}' is not a valid operation.")
