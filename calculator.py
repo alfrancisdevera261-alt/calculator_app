0be65e415c15241b4852b169a3caf672e0a36cd4
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-05-06 20:39:48 +0800

    imported exceptions to use where the rules of the calculators

--- calculator.py
+++ calculator.py
@@ -2,3 +2,10 @@ from operations.operation import (
     Addition, Subtraction, Multiplication,
     Division, Modulo, Exponentiation
 )
+
+from operations.exceptions import (
+    InvalidOperationError,
+    InvalidNumberError,
+    DivisionByZeroError,
+    CalculatorError
+)
