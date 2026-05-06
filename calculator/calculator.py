06cd2670008e198d334253e99bcd116de85b52c7
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-05-06 20:46:12 +0800

    created a menu dictionary that maps user choice of operator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -30,3 +30,12 @@ class Calculator:
     - Custom exception handling for invalid inputs
     - Stores all calculations in history
     """
+    
+MENU = {
+        "1": Addition(),
+        "2": Subtraction(),
+        "3": Multiplication(),
+        "4": Division(),
+        "5": Modulo(),
+        "6": Exponentiation(),
+    }
